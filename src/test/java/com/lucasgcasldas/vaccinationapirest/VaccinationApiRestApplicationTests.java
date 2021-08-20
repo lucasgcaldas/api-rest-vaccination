6443343d8355b8217b484e8c9490a4db95b9bf60
Lucas Gomes <lggomes007@gmail.com>
2021-08-20 20:24:35 -0300

    Starting a project

---
+++ src/test/java/com/lucasgcasldas/vaccinationapirest/VaccinationApiRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasgcasldas.vaccinationapirest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaccinationApiRestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
